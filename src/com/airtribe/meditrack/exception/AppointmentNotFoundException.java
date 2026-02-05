0432c79bc45daa0de2d0176d9a33180ba6052114
Swarajyalakshmi <mslakshmijaya@gmail.com>
2026-02-05 22:48:56 +0530

    skeleton of the Medi track project

---
+++ src/com/airtribe/meditrack/exception/AppointmentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.airtribe.meditrack.exception;
+
+public class AppointmentNotFoundException extends Exception {
+    public AppointmentNotFoundException(String message) {
+        super(message);
+    }
+
+}
